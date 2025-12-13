e04f99feb15ae885c0a492e34b3a2b5c0a2aa04b
NorbertDerHorvath <horvath.81.norbert@gmail.com>
2025-12-13 11:13:57 +0100

    sosem fog működni

--- server/models/index.js
+++ server/models/index.js
@@ -3,18 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '..', 'config', 'database.js'))[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// Directly import the already configured sequelize instance from the custom database.js
+const sequelize = require(path.join(__dirname, '..', 'config', 'database.js'));
 
 fs
   .readdirSync(__dirname)
@@ -37,6 +30,7 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
+// Explicitly define associations
 db.User.belongsTo(db.Company, { foreignKey: 'companyId', as: 'Company' });
 db.Company.hasMany(db.User, { foreignKey: 'companyId' });
 
