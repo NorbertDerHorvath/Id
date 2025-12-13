dda4de885ec0b9d2391e2449b78f85999fb30f6b
NorbertDerHorvath <horvath.81.norbert@gmail.com>
2025-12-13 09:28:11 +0100

    semmi reményem

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,22 @@
-const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_DATABASE || 'idobata',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   }
-});
-
-module.exports = sequelize;
+};
