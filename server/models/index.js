4bced90db7b8c683bbc0deac3048620ba0b8eb85
NorbertDerHorvath <horvath.d.norbert@gmail.com>
2025-12-26 20:39:55 +0100

    néhány hét kihagyás után...

--- server/models/index.js
+++ server/models/index.js
@@ -4,10 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(path.join(__dirname, '..', 'config', 'database.js'))[env];
 const db = {};
 
-// A már konfigurált sequelize példány importálása
-const sequelize = require(path.join(__dirname, '..', 'config', 'database.js'));
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs.readdirSync(__dirname)
   .filter(file => {
